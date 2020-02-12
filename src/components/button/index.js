e102b6dd96e6ed7476dabe9910754aa5d963eb87
julienjugepro <35448079+julienjugepro@users.noreply.github.com>
2020-02-12 15:21:15 +0100

    Revert "Update julien-organization.julien-collection/button-git to its latest version"

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -3,7 +3,11 @@ import "./button.scss";
 import React from "react";
 
 const Button = ({ children }) => {
-  return <button className="julien-button">{children}</button>;
+  return (
+    <button style={{ color: "red" }} className="julien-button">
+      {children}
+    </button>
+  );
 };
 
 export default Button;
